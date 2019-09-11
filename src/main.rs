c529800bdb62506487be03c2794759bb6c472e16
k-nasa <htilcs1115@gmail.com>
2019-09-12 00:19:38 +0900

    impl base

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 #![feature(async_closure)]
 
+async fn circles(_: tide::Context<()>) -> tide::EndpointResult {
+    Ok(tide::response::json(""))
+}
+
 fn main() {
     let mut app = tide::App::new();
 
-    app.at("/").get(async move |_| "Hello, world!");
+    app.at("/api/circles").get(circles);
     app.run("127.0.0.1:8080").unwrap();
 }
