0e09d63e9463d214843c6e8a5ebbf980d5fe2c69
k-nasa <htilcs1115@gmail.com>
2019-09-13 23:58:08 +0900

    feat: pick response json field

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,38 @@
 #![feature(async_closure)]
 
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Circles {
+    pub list: Vec<Circle>
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all(serialize = "camelCase", deserialize = "camelCase"))]
+struct Circle {
+    pub id: String,
+    pub name: String,
+    pub name_ruby: String,
+    pub pen_name: String,
+    pub genre: String,
+    pub genre_free_format: String,
+    pub circle_cut_image: Option<CircleCutImage>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct CircleCutImage {
+    url: String,
+}
+
 async fn circles(_: tide::Context<()>) -> tide::EndpointResult {
     let mut res = surf::get("https://techbookfest.org/api/circle?eventID=tbf07&limit=642").await.unwrap();
+
     let json_string = res.body_string().await.unwrap();
     let data: serde_json::Value = serde_json::from_str(&json_string).unwrap();
+    let circles: Circles = serde_json::from_value(data.clone()).unwrap();
 
-    Ok(tide::response::json(data))
+
+    Ok(tide::response::json(circles))
 }
 
 fn main() {
